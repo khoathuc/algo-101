5682e0b50e01ac2ca6d7b662942062a0a3acbe6b
khoathuc <khoathuc141201@gmail.com>
2024-05-02 15:08:03 +0700

    partition label

--- LeetCode/greedy/partition-labels/partition-labels.py
+++ LeetCode/greedy/partition-labels/partition-labels.py
@@ -29,8 +29,59 @@ class Solution():
     We'll solve problems here
     """
     def solve(self, s: str):
-        print(s)
+        last: dict = dict()
+        parts: List[str] = []
+
+        # use visited to check if char is visited or not
+        visited: dict = dict()
+
+        # mark all visited char as false initially
+        # store last index of that char in s
+        for i in range (len(s)):
+            last[s[i]] = i
+            visited[s[i]] = False
         
+        #start from first letter, find minimal partition contain first letter
+        #update the partition
+        part: str = ""
+        end_part: int = 0
+        start_part: int = 0
+
+        index = 0
+        while index <= len(s):
+            if(index == len(s)):
+                parts.append(len(part))
+                break
+
+            if(visited[s[index]]):
+                index+=1
+                continue
+            else:
+                visited[s[index]] = True
+                
+            last_char_index = last[s[index]]
+            #if last char index < end_part, do not need to update the part
+            if(last_char_index < end_part):
+                index+=1
+                continue
+            
+            if(index > end_part):
+                #push part in parts
+                parts.append(len(part))
+
+                #init new part, start from index => last_index
+                start_part = index
+                part = s[start_part:last_char_index + 1]
+                end_part = last_char_index
+            else:
+                #compare end_part with last_index of s[i]
+                if(end_part <= last_char_index):
+                    part = s[start_part:last_char_index + 1]
+                    end_part = last_char_index
+            
+            index+=1
+        return parts
+
 
 def app():
     """Function read input, solve and output"""
